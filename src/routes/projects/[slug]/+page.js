40593afbeb5256d920ba0a5efcadafa450bf8f10
FrostDev <frostdev123friday@gmail.com>
2025-05-13 16:43:05 -0400

    fix export projects (again)

--- src/routes/projects/[slug]/+page.js
+++ src/routes/projects/[slug]/+page.js
@@ -3,3 +3,7 @@ export const load = ({ params }) => {
         slug: params.slug
     }
 }
+
+import p_data from "$lib/data/projects.json"
+export const entries = p_data.projects.map(p => `/projects/${p.name}`);
+console.log(entries)
