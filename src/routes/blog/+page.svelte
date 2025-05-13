ac9790d06148126e3505c9060c50e4f270630d11
FrostDev <frostdev123friday@gmail.com>
2025-05-13 16:26:15 -0400

    add temp blog path

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col gap-7 justify-center items-center px-8 w-full">
+    <div class="px-20 w-full max-w-7xl">
+        <h2 class="font-mono text-7xl text-bold">WIP</h2>
+        <p class="text-zinc-600"> coming soon...</p>
+    </div>
+</div>
