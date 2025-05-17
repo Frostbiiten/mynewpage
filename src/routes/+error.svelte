6451f7ac3dfe5762af3212d001e78e00ea9e9e33
FrostDev <frostdev123friday@gmail.com>
2025-05-17 12:33:49 -0400

    update 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,10 +2,17 @@
 import { page } from '$app/stores';
 </script>
 
-<div class="flex flex-col gap-7 justify-center items-center px-8 w-full">
-    <div>
-        <h2 class="text-7xl">404</h2>
-        <p class="text-zinc-700">Page not found !</p>
+<div class="flex flex-col gap-7 justify-center items-center -mt-5 w-full h-[calc(100vh-15rem)] -mb-10">
+    <div class="px-8 w-full max-w-7xl h-full">
+        <div class="flex flex-col relative justify-center items-center w-full h-full border-dashed border-3 border-gray-600 rounded-lg bg-radial-[at_50%_0%] to-150% from-[#0d1117]/100 to-[#0a0d0f]/100">
+            <h2 class="text-8xl font-black">404</h2>
+            <p class="-mt-1 text-zinc-600">Page not found !</p>
+            <div class="absolute top-0 left-0 w-full h-full overflow-clip mix-blend-multiply opacity-4">
+                <div class="overflow-hidden relative w-full h-full rounded-xl scale-300">
+                    <div class="absolute top-0 left-0 w-full h-full opacity-10 grain z-9999">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-
